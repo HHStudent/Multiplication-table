494ce747e50591273fb755a530d6c5b67b641011
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-10-20 14:42:05 -0400

    Updated from Brython Server: 10/20/2015 2:42:05 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
 Author: Dimitri
-Credit: Mr. Dennison
+Credit: Mr. Dennison, Adam Pikielny
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -25,5 +25,5 @@ w = int(input("Width of multiplication table: "))
 h = int(input("Height of multiplication table: "))
 for x in range(1, h+1):
     for y in range(1, w+1):
-        print("{0:>3}".format(x*y), end="")
+        print(" {0:>3}".format(x*y), end="")
     print('\n')
